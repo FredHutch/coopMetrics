61bf5f91fd8a94fe0d11224b0205d3a6b3e061ce
lwolfe <lwolfe@fredhutch.org>
2020-08-06 13:52:51 -0700

    create script to pull data that will be cached in the package

---
+++ data-raw/DATA.R
@@ -0,0 +1,15 @@
+library(coopMetrics)
+
+# Set variables
+owner <- "FredHutch"
+repo <- "coop"
+
+# Code to prepare known contributor dataset
+knownContributorData <- listFilesandEarliestCommitDate(owner = owner,
+                                                       repo = repo,
+                                                       path = "_contributors",
+                                                       ordered = TRUE)
+knownContributorData$handle <- path2Contributor(contributorData$path)
+cacheDate <- Sys.time()
+
+usethis::use_data(knownContributorData, cacheDate, internal = TRUE, overwrite = FALSE)
