e763668b2c5e0b7586c650d7ee2c1fbf9105984f
lwolfe <lwolfe@fredhutch.org>
2020-08-11 13:29:42 -0700

    rerun contributor cache to update commitDate output

--- data-raw/DATA.R
+++ data-raw/DATA.R
@@ -9,7 +9,7 @@ knownContributorData <- listFilesandEarliestCommitDate(owner = owner,
                                                        repo = repo,
                                                        path = "_contributors",
                                                        ordered = TRUE)
-knownContributorData$handle <- path2Contributor(contributorData$path)
+knownContributorData$handle <- pathToContributor(knownContributorData$path)
 cacheDate <- Sys.time()
 
-usethis::use_data(knownContributorData, cacheDate, internal = TRUE, overwrite = FALSE)
+usethis::use_data(knownContributorData, cacheDate, internal = TRUE, overwrite = TRUE)
