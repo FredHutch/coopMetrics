fa3ff0e2960db02e2442790abce1c50b6fb5ee20
lwolfe <lwolfe@fredhutch.org>
2021-01-19 15:23:28 -0800

    first test number commites

---
+++ tests/testthat/test-github.R
@@ -0,0 +1,14 @@
+context("GitHub API and related functions")
+library(coopMetrics)
+library(lubridate)
+library(tidyverse)
+
+commit_df <- calcCommitNum(owner = "FredHutch",
+                           repo = "coopMetrics",
+                           dateRange = c("2020-06-01", "2020-08-01"))
+
+test_that("calcCommitNum calculates the number of commits per month", {
+  expect_equal(length(commit_df$month), 3)
+  expect_equal(commit_df$numCommits, c(6, 17, 53))
+  expect_equal(commit_df$month, as_date(c("2020-06-01", "2020-07-01", "2020-08-01")))
+})
