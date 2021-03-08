6ed4c34c75eca8012b3cd07ab0f4fdd8d2575de1
lwolfe <lwolfe@fredhutch.org>
2021-03-08 14:29:11 -0800

    test

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -10,6 +10,6 @@ test_that("calcCommitNum calculates the number of commits per month", {
 
   expect_equal(length(commit_df$month), 3)
   #not sure why this one is constantly failing??
-  #expect_equal(commit_df$numCommits, c(8, 17, 53))
+  #expect_equal(commit_df$numCommits, c(9, 17, 53))
   expect_identical(commit_df$month, as_date(c("2020-06-01", "2020-07-01", "2020-08-01")))
 })
