ee7b5f02e89ae42ec3440e71fdaf5f0f4983786a
lwolfe <lwolfe@fredhutch.org>
2021-03-18 10:48:02 -0700

    move generic functions to utils

--- R/pullGithub.R
+++ R/pullGithub.R
@@ -33,7 +33,7 @@ githubMetrics <- function(owner,
   post <- postMetrics(owner = owner,
                       repo = repo,
                       dateRange = dateRange)
-
+  # merge
   githubData <- left_join(contributor, commit, by = "month") %>%
     left_join(., post, by = "month") %>%
     select(month, numCommits, numPostTotal, numNewPosts, totalContributors, numNewContributors, handles)
@@ -288,8 +288,6 @@ calcPostNumJekyll <- function(posts,
   return(postTbl)
 }
 
-
-
 ## OTHER HELPER FUNCTIONS ----------------------------------------------------------------
 
 #' Get the names of files in a specified directory in a github repo
@@ -360,54 +358,4 @@ filepathToOldestCommitDate <- function(owner,
   # most recent commit is always the first row. Oldest commit is last.
   oldestCommitDate <- as_date(commitObj[[max(commitNum)]]$commit$author$date)
   return(oldestCommitDate)
-}
-
-#' Fill in missing months in a vector or dataframe column. If a dataframe is supplied the extra columns will be filled in with NA. Dates should be a date object.
-#' @param data A vector or dataframe of dates in yyyy-mm-dd format. Filled in dates will be for the first of the month.
-#' @param dateCol if a dataframe is supplied, what is the column name of the dates variable. Defaults to "month".
-
-completeMonths <- function(data,
-                           dateCol = NULL) {
-  # check if data is a dataframe
-  isDf <- is.data.frame(data)
-  if (isDf) {
-    dates <- data[[dateCol]]
-  } else {
-    dates <- data
-  }
-  # check that we're working with dates
-  if (!is.Date(dates)) {
-    stop("Provided vector or column is not in date format")
-  }
-  # if it's a dataframe return the specified date column
-  # if it's a vector return
-  completeRange <-  tibble(month = seq.Date(from = min(dates),
-                                            to = max(dates),
-                                            by = "month"))
-  if (isDf) {
-    res <- full_join(data, completeRange, by = "month")
-    res <- res[order(res$month),]
-  } else {
-    res <- completeRange[["month"]]
-  }
-  return(res)
-}
-
-## Date handling
-is.Ymd <- function(x) {
-  !is.na(lubridate::ymd(x, quiet = TRUE))
-}
-
-checkDateFormat <- function(dates) {
-  isYmd <- sapply(dates, is.Ymd)
-  if (!all(isYmd)) {
-    stop("ERROR: Dates must be in year-month-date format")
-  }
-}
-
-dateRangeToStartEnd <- function(dateRange) {
-  start <- floor_date(min(dateRange), "month")
-  end <- ceiling_date(max(dateRange), "month") - 1
-
-  return(list(start = start, end = end))
 }
